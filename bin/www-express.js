d617f3f3473f2e7f6f7e7899fd4dcbce2e19f7b6
Ronald Uitermark <ronald645@gmail.com>
2018-03-03 07:30:28 +0100

    Finish CRUD example, implement cookie example.

--- bin/www-express.js
+++ bin/www-express.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const bodyParser= require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const session = require('express-session');
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}))
-app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+var publicpath = path.resolve(__dirname + '/../');
+app.use(express.static(publicpath));
 
 var db;
 
@@ -32,7 +36,7 @@ app.get('/', (req, res) => {
   });
   //var index = path.resolve(__dirname + '/../public/index.html');
   //res.sendFile(index);
-  
+
   // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
   // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
 });
@@ -44,3 +48,44 @@ app.post('/quotes', (req, res) => {
       res.redirect('/');
     })
 });
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({name: 'Yoda'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/quotes', (req, res) => {
+  db.collection('quotes').findOneAndDelete({name: req.body.name},
+  (err, result) => {
+    if (err) return res.send(500, err)
+    res.send({message: 'A darth vadar quote got deleted'})
+  })
+})
+
+// Use the session middleware
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+
+// Access the session as req.session
+app.get('/cookietest', function(req, res, next) {
+  if (req.session.views) {
+    req.session.views++
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>views: ' + req.session.views + '</p>')
+    res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+    res.end()
+  } else {
+    req.session.views = 1
+    res.end('welcome to the session demo. refresh!')
+  }
+})
